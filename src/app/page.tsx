f03c4b3412b88b1bb27f38c153fa0bef1fd25f3f
MUZAAQI <muzaaqi@yahoo.com>
2025-11-20 22:20:47 +0700

    chore(ui): Improve Home page layout and responsiveness
    Refactored the main container to use flex-col-reverse for better mobile layout and added md:flex-row for desktop. Updated heading font sizes for responsiveness and enhanced visual appearance with drop shadows.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
   return (
     <div className="bg-background flex min-h-screen items-center justify-center font-sans">
       <main className="container flex min-h-screen w-full flex-col items-center justify-center">
-        <div className="border-border flex gap-2 border-b-2 px-5">
+        <div className="border-border md:flex-row drop-shadow-primary/20 flex flex-col-reverse gap-2 border-b-2 px-5 drop-shadow-lg">
           <Image
             src="/profile.png"
             alt="Description"
             width={300}
             height={300}
           />
-          <div className="self-end pl-8 mb-5 space-y-5">
+          <div className="mb-5 space-y-5 self-end md:pl-8">
             <div>
-              <h1 className="text-foreground/80 mb-4 text-6xl font-bold">
+              <h1 className="text-foreground/80 mb-4 text-4xl md:text-6xl font-bold">
                 Muhammad
               </h1>
-              <h1 className="text-foreground/80 mb-4 text-5xl font-bold">
+              <h1 className="text-foreground/80 mb-4 text-4xl md:text-5xl font-bold">
                 <span className="text-foreground underline">Zaki</span> As
                 Shidiqi
               </h1>
