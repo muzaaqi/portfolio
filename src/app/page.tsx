803f895004b76d9666f53a7bf7f244d929867162
MUZAAQI <muzaaqi@yahoo.com>
2025-11-20 22:28:53 +0700

    chore(ui): Update layout and heading styles in Home page
    Reordered class names for better readability and consistency, adjusted heading font sizes for improved visual hierarchy, and modified alignment classes for responsive design in the Home page component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ export default function Home() {
   return (
     <div className="bg-background flex min-h-screen items-center justify-center font-sans">
       <main className="container flex min-h-screen w-full flex-col items-center justify-center">
-        <div className="border-border md:flex-row drop-shadow-primary/20 flex flex-col-reverse gap-2 border-b-2 px-5 drop-shadow-lg">
+        <div className="border-border drop-shadow-primary/20 flex flex-col-reverse gap-2 border-b-2 drop-shadow-lg md:flex-row px-5">
           <Image
             src="/profile.png"
             alt="Description"
             width={300}
             height={300}
           />
-          <div className="mb-5 space-y-5 self-end md:pl-8">
+          <div className="mb-5 space-y-5 md:self-end md:pl-8">
             <div>
-              <h1 className="text-foreground/80 mb-4 text-4xl md:text-6xl font-bold">
+              <h1 className="text-foreground/80 mb-4 text-5xl font-bold md:text-6xl">
                 Muhammad
               </h1>
-              <h1 className="text-foreground/80 mb-4 text-4xl md:text-5xl font-bold">
+              <h1 className="text-foreground/80 mb-4 text-4xl font-bold md:text-5xl">
                 <span className="text-foreground underline">Zaki</span> As
                 Shidiqi
               </h1>
