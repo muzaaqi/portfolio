20935e66fd50c01bd7094888dfa59146648de09e
MUZAAQI <muzaaqi@yahoo.com>
2026-02-15 18:39:55 +0700

    feat: Add Google Tag Manager and remove gtag
    Integrate Google Tag Manager by injecting the GTM script in the head (afterInteractive) and adding the noscript iframe fallback in the body. Remove the previous Google Analytics gtag script and inline config. Structured data and theme/provider code remain unchanged.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -226,10 +226,25 @@ export default async function RootLayout({
           crossOrigin="anonymous"
         />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TS65PZH9');`}
+        </Script>
       </head>
       <body
         className={`${inter.variable} ${jetBrainsMono.variable} ${anton.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TS65PZH9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -248,19 +263,6 @@ export default async function RootLayout({
             __html: JSON.stringify(profilePageSchema),
           }}
         />
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-DQ9JB8BVDX"
-          strategy="lazyOnload"
-        />
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-DQ9JB8BVDX');
-          `}
-        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
