7fca05e2f9a6b635f71d6aa9cbeba1425f60a859
MUZAAQI <muzaaqi@yahoo.com>
2026-02-22 10:01:41 +0700

    refactor: Use env GTM ID and add Cloudflare Insights
    Replace hardcoded Google Tag Manager ID with NEXT_PUBLIC_GTM_ID so the GTM container can be configured via environment variables, and update the noscript iframe URL accordingly. Add Cloudflare Insights beacon script (deferred) using NEXT_PUBLIC_CLOUDFLARE_INSIGHTS_TOKEN via data-cf-beacon for site telemetry. Minor formatting/indentation adjustments applied to the inline GTM Script.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -228,18 +228,24 @@ export default async function RootLayout({
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TS65PZH9');`}
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');`}
         </Script>
+
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon={JSON.stringify({token: process.env.NEXT_PUBLIC_CLOUDFLARE_INSIGHTS_TOKEN})}
+        />
       </head>
       <body
         className={`${inter.variable} ${jetBrainsMono.variable} ${anton.variable} antialiased`}
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TS65PZH9"
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
